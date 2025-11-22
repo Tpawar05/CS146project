4e0c3e7e58f4805b7640006a5f9a1f5af7771090
MasroorHReal <masroorhaider70@gmail.com>
2025-11-22 16:16:21 -0500

    Tried adding test page js

---
+++ CS146project_backup/testPage.js
@@ -0,0 +1,37 @@
+document.getElementById("eventForm").addEventListener("submit",function(e){
+
+    {
+e.preventDefault();
+console.log("FORM SUBMITTED,SHOULD SUBMIT DATA!!!!")
+
+const date=document.getElementById("eventDate").value;
+const desc=document.getElementById("eventDescription").value;
+const importance=document.getElementById("eventImportance").value;
+const color=document.getElementById("eventColor").value;
+
+const newEvent={
+    date,description:desc,importance,color
+};
+
+
+console.log("THE NEW EVENT YOU MADE:",newEvent);
+
+const eventDiv=document.createElement("div");
+
+/*supposed to copy the css for task */
+eventDiv.classList.add("task");
+
+eventDiv.style.backgroundColor=newEvent.color;
+
+eventDiv.textContent="Date:"+newEvent.date+"\nDescription:"+newEvent.description+"\nImportance:"+newEvent.importance;
+
+const container=document.getElementById("userEvents");
+container.appendChild(eventDiv)
+console.log("ADDED THE EVENT!")
+this.reset();
+
+}
+    
+
+})
+
